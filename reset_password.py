3c2866b4b279dc93ea45205819161596cc2e5cb7
urlordjames <urlordjames@gmail.com>
2021-03-23 10:15:40 -0400

    added option for different hostname

--- reset_password.py
+++ reset_password.py
@@ -1,20 +1,26 @@
 import requests
 
-token = input("auth token:\n")
+def reset_passwd(hostname, token, user, password):
+	headers = {"Authorization": "Bearer " + token}
+	
+	url = f"https://{hostname}/_synapse/admin/v1/reset_password/{user}"
+	
+	params = {
+		"new_password": password,
+		"logout_device": True
+	}
+	
+	r = requests.post(url, json=params, headers=headers)
+	return r
 
-user = input("matrix user id:\n")
-password = input("password:\n")
+if __name__ == "__main__":
+	hostname = input("hostname:\n")
+	token = input("auth token:\n")
+	uname = input("username to reset password of:\n")
+	user = f"@{uname}:{hostname}"
+	password = input("password:\n")
 
-headers = {"Authorization": "Bearer " + token}
-
-url = f"https://jamesvps.tk/_synapse/admin/v1/reset_password/{user}"
-
-params = {
-	"new_password": password,
-	"logout_device": True
-}
-
-r = requests.post(url, json=params, headers=headers)
-
-print(r.status_code)
-print(r.text)
+	r = reset_passwd(hostname, token, user, password)
+	
+	print(r.status_code)
+	print(r.text)
